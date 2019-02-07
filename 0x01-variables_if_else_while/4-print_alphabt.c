f07acd56961c7dc7aa12f1b19797e1d5c3405ad5
renefibonacci660 <660@holbertonschool.com>
2019-02-07 23:06:15 +0000

    Fixing 3, 3rd commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point of function
+ * Description: prints a - z then newline
+ * Return: Always zero
+ */
+int main(void)
+{
+char a;
+char b;
+
+a = 'a';
+b = '\n';
+
+while (a <= 'z')
+{
+	putchar(a);
+
+	if (a == 'p' || a == 'd')
+	{
+		a = a + 2;
+	}
+	else
+	{
+		a = a + 1;
+	}
+}
+	putchar(b);
+	return (0);
+}
