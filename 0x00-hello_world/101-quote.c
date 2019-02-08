e21d3b080e3bf36f2102457d577345a5019fe4df
renefibonacci660 <660@holbertonschool.com>
2019-02-08 23:46:28 +0000

    Adding 9, first commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of function
+ * Description: prints text to standard error
+ * Return: Always zero
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, "- Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
